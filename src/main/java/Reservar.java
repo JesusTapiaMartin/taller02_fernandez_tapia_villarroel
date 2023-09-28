5be8841016e810d986a0311b99f0801ec0adb2fd
Benttyman <jesustapiamartin@gmail.com>
2023-09-28 10:42:43 -0300

    Correciones

--- src/main/java/Reservar.java
+++ src/main/java/Reservar.java
@@ -1,40 +1,6 @@
 import java.util.Scanner;
 public class Reservar {
 
-    public static void main(String[] args) {
-        String[][] hotel = crearMatriz();
-        mostrarHotel(hotel);
-        String[] reserva = reservarHabitacion(hotel);
-        confimarReserva(hotel, reserva[0], reserva[1], reserva[2]);
-        mostrarHotel(hotel);
-    }
-    //=================== CREAR MATRIZ ===================
-    public static String[][] crearMatriz() {
-        String[][] hotel = new String[10][4];
-
-        for (int i = 0; i < 10; i++) {
-            hotel[i][0] = Integer.toString(i + 1);
-            hotel[i][1] = "Disponible";
-            hotel[i][2] = "0";
-            hotel[i][3] = "Sin alimentación";
-        }
-
-        return hotel;
-    }
-
-    // =================== MOSTRAR HOTEL ===================
-    public static void mostrarHotel(String[][] hotel) {
-        for (int i = 0; i < hotel.length; i++) {
-            for (int j = 0; j < hotel[i].length; j++) {
-                System.out.print(hotel[i][j] + "\t\t\t");
-
-            }
-            System.out.println();
-        }
-    }
-
-
-
     //=================== RESERVAR HABITACIÓN ===================
     public static String[] reservarHabitacion(String[][] hotel) {
         Scanner lector      = new Scanner(System.in);
