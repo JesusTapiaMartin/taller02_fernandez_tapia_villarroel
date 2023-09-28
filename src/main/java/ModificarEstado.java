e1a305f355a9ea76ef02bef94efbc2bdf9c29371
GatoMiau03 <d.benaldo03@ufromail.cl>
2023-09-28 10:17:56 -0300

    Metodo de modificar estado de las habitaciones

---
+++ src/main/java/ModificarEstado.java
@@ -0,0 +1,24 @@
+public class ModificarEstado {
+
+    public static void main(String[] args) {
+        String[][] habitaciones = new String[][]{
+                {"1", "libre"},
+                {"2", "ocupada"},
+                {"3", "ocupada"}
+        };
+        System.out.println("La habitacion numero: " + habitaciones[0][0] + ", Tiene el estado: " + habitaciones[0][1]);
+
+        modificarEstado(habitaciones, "1", "ocupada");
+        System.out.println("La habitacion numero: " + habitaciones[0][0] + ", Se actualizo al estado: " + habitaciones[0][1]);
+    }
+    public static void modificarEstado(String[][] habitaciones, String NumeroHabitacion, String EstadoHabitacion) {
+        for (int i = 0; i < habitaciones.length; i++) {
+            if (habitaciones[i][0].equals(NumeroHabitacion)) {
+                habitaciones[i][1] = EstadoHabitacion;
+            }
+        }
+    }
+// Se tiene que modificar este for de acuerdo a como se creará la matriz
+
+
+}
